7fb8ec751814d687f4eb4b25875205f81ea04421
morgangrace11 <morgangrace720@gmail.com>
2019-01-27 16:05:24 -0800

    getting correct data needed

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -9,28 +9,48 @@ class App extends React.Component {
 
     this.state = {
       list: [],
-      date: '',
+      time: '',
       event: '',
-      toggle: false
+      toggle: false,
+      date: ''
     }
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleEventChange = this.handleEventChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
   }
 
-  handleClick() {
+  handleClick(e) {
+    console.log(e.target.innerHTML)
     this.setState({
-      toggle: !this.state.toggle
+      toggle: !this.state.toggle,
+      date: `2/${e.target.innerHTML}/2019`
+    })
+  }
+
+  handleEventChange(e) {
+    console.log(e.target.value)
+    this.setState({
+      event: e.target.value
+    })
+  }
+
+  handleTimeChange(e) {
+    console.log(e.target.value)
+    this.setState({
+      time: e.target.value
     })
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1 id="title">Kitty Flitter</h1>
         <main>
           <Calendar handleClick={this.handleClick} />
           <List />
-          {this.state.toggle ? <Popup handleClick={this.handleClick} /> : null}
+          {this.state.toggle ? <Popup handleClick={this.handleClick} handleEventChange={this.handleEventChange} handleTimeChange={this.handleTimeChange} /> : null}
         </main>
       </div>
     );
--- client/src/components/popup.jsx
+++ client/src/components/popup.jsx
@@ -5,12 +5,12 @@ const Popup = (props) => (
     <div id="popup-main">
       <form>
         Event:
-    <input type="text" />
+    <input type="text" onChange={props.handleEventChange} required />
         <br />
         Time:
-    <input type="text" />
+    <input type="time" onChange={props.handleTimeChange} required />
         <br />
-        <input onClick={props.handleClick} type="submit" />
+        <input onClick={props.handleClick} type="button" value="submit" />
       </form>
       <button onClick={props.handleClick} >cancel</button>
     </div>
