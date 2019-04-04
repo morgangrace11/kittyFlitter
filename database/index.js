8965ec7a704ac1f5a4adc04a9a5a2c0ab0dc61f3
morgangrace11 <morgangrace720@gmail.com>
2019-04-03 20:23:46 -0400

    changed users table

--- database/index.js
+++ database/index.js
@@ -48,6 +48,12 @@ sequelize
   })
 
   const User = sequelize.define('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
     username: {
       type: Sequelize.STRING,
       allowNull: false,
